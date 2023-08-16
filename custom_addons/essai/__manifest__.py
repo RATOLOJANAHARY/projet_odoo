b85e4db6ee212f948fce427433bee7dafaae33fc
Andre RATOLOJANAHARY <rjandrejean@gmail.com>
2023-08-16 13:28:54 +0300

    test

--- custom_addons/essai/__manifest__.py
+++ custom_addons/essai/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "essai",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        teshsghdfgjugviurvoyovtm""",
 
     'description': """
         Long description of module's purpose
